1c95edd846fb497c198c943d76970af6e60bec3e
Ian Swartz <ian.s.swartz@gmail.com>
2026-02-13 23:16:42 +0000

    display_schedule: worked on displaying and exporting schedule to csv file (txt file version commented out

--- app/schedule_viewing/schedule_viewing.py
+++ app/schedule_viewing/schedule_viewing.py
@@ -12,9 +12,51 @@ Related User Stories:
     D1 — Display Schedules in CSV Format
 """
 
+import csv
+import sys
 from typing import Dict, Any
 
+# A function that displays the schedule as a csv file.
+# Takes a list of schedule entruies and prints them in a CSV format.
+def display_schedule_as_csv(schedule_data: List[Dict[str, Any]]) -> None:
+    if not schedule_data:
+        print("No schedule data available to display.")
+        return
+    
+    headers = schedule_data[0].keys()
 
+    # Use csv.DictWriter function so that it's formatted correctly, and prints to terminal.
+    writer = csv.DictWriter(sys.stdout, fieldnames=headers)
+    
+    # Print the header row (e.g., course_id, room, time).
+    writer.writeheader()
+    
+    for row in schedule_data:
+        writer.writerow(row)
+
+
+# A function that exports the schedule to a csv file.
+def export_schedule_to_csv_file(schedule_data: List[Dict[str, Any]], filename: str = "schedule_output.csv") -> None:
+    try:
+        if not schedule_data:
+            raise ValueError("No data to save.")
+            
+        headers = schedule_data[0].keys()
+
+        with open(filename, mode='w', newline='') as f:
+            writer = csv.DictWriter(f, fieldnames=headers)
+            writer.writeheader()
+            writer.writerows(schedule_data)
+        print(f"\nSuccessfully exported schedule to {filename}")
+
+    except Exception as e:
+        print(f"Error exporting to CSV: {e}")
+
+
+
+
+# Commented out cause we want a CSV not a text file.
+"""
 # A function used to display a scheulde.
 # Takes the output dictionary from the scheduler and prints it.
 def display_schedule(schedule_data: Dict[str, Any]) -> None:
@@ -37,7 +79,6 @@ def display_schedule(schedule_data: Dict[str, Any]) -> None:
     
     print("="*50 + "\n")
 
-
 # A function that exports the schedule to a text file.
 def export_schedule_to_text(schedule_data: Dict[str, Any], filename: str = "final_schedule.txt") -> None:
     try:
@@ -52,4 +93,5 @@ def export_schedule_to_text(schedule_data: Dict[str, Any], filename: str = "fina
     
     except Exception as e:
         print(f"Failed to export schedule: {e}")
+"""
 
