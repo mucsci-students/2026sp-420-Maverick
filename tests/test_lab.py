c7cba59dce278387c41d9437ddf8f3765b8f6ebf
Tanner <tvness@millersville.edu>
2026-02-15 10:11:52 -0500

    tests for removing from course, faculty, lab, and room

--- tests/test_lab.py
+++ tests/test_lab.py
@@ -1,5 +1,5 @@
-# Author: 
-# Date: <yyyy-mm-dd>
+# Author(s): Tanner Ness
+# Date: 2026-02-14
 """
 test_lab.py
 
@@ -31,3 +31,34 @@ Related User Stories:
     A3.2 — Modify Lab
     A3.3 — Delete Lab
 """
+from ..app.lab_management import lab_management
+import json
+
+def get_example():
+    with open('..configs/config_base.json', 'r') as file:
+        return json.load(file)
+    
+example = get_example().copy()
+
+# the lab should be removed from 'lab'
+def delete_lab():
+    lab = 'Lab 2'
+    lab_management.remove_lab(example, lab)
+
+    assert lab not in example['config']['labs'], f"Lab {lab} has not been removed from 'lab'."
+
+# the lab should be removed from 'lab' and 'courses'
+def delete_lab_nested():
+    lab = 'Lab_1'
+    lab_management.remove_lab(example, lab)
+
+    assert lab not in example['config']['labs'], f"Room {lab} has not been removed from 'room'."
+
+    assert lab not in any(l['lab'] == lab for l in example['config']['courses']), f"Room {lab} has not been removed from 'courses'."
+
+# should raise an error
+def delete_lab_nonexistent():
+    try:
+        lab_management.remove_lab(example, 'Lab 999')
+    except ValueError:
+        print(f"Removing a nonexistent lab raises the correct error: {ValueError}")
